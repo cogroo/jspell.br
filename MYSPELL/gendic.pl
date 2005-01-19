3c77e16d6ef52edf51c5d6337387085646eb682c
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2005-01-19 14:57:21 +0000

    Fixes.
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@1958 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- MYSPELL/gendic.pl
+++ MYSPELL/gendic.pl
@@ -1,38 +1,39 @@
 #!/usr/bin/perl -w
 
 #Generates PT MySpell dictionary from PT JSpell & ISpell
-#Last version2005
-
+#Last version 19/01/2005
 
 use strict;
 use locale;
 
-my $pri; my $seg; my @ter; my @qua; my @qui; my $i; my $tmp; my $tmp2; 
+my $pri; my $seg; my @ter; my @qua; my @qui; my $i; my $tmp; my $tmp2; my $flg; 
 
 print "SET ISO8859-1\n";
-#print "TRY ãáéêèíõóôâàúçesianrtolcdugmphbfv\n\n";
 #linha a seguir gerada dinamicamente (lista de letras ordenadas pelo número de ocorrências
-print "TRY aerisontcdmlupvgbfzáhçqjíxãóéêâúõACMPSBTELGRIFVDkHJONôywUKXZWQÁYÍÉàÓèÂÚ";
+print "TRY aerisontcdmlupvgbfzáhçqjíxãóéêâúõACMPSBTELGRIFVDkHJONôywUKXZWQÁYÍÉàÓèÂÚ\n\n";
 
 
 while(<>){
     next if (/^wordchars/ || /^\s+$/ || /^\#/ || /^defstringtype/ || /^allaffixes/ );
     if (/^prefixes$/){$pri='PFX';next;}
     if (/^suffixes$/){$pri='SFX';next;}
-    if (/^flag [\*\+]?(\w)/) {    ##Flag data
-	$tmp=$1;
+    if (/^flag ([\*\+]?)(\w)/) {    ##Flag data
+	$tmp=$2;
+	$tmp2=$1;
+	
 	if (defined($seg)){
-	    printf ("\n%s %s Y %d\n",$pri,$seg,$#ter+1); ##number of items
+	    printf ("\n%s %s %s %d\n",$pri,$seg,$flg=~/[*+]/ ? 'Y' : 'N',$#ter+1); ##number of items
 	    for ($i=0;$i<@qui;$i++){
-		print "$pri $seg   ".lc($ter[$i])."\t\t".lc($qua[$i])."\t\t".lc($qui[$i])."\n";
+		print "$pri $seg   ".lc($ter[$i])." " x (15-length($ter[$i])).lc($qua[$i])." " x (15-length($qua[$i])).lc($qui[$i])."\n";
 	    }
 	    @ter=();@qua=();@qui=();
 	}
 	$seg=$tmp;
+	$flg=$tmp2;
 	next;
     }
     s/\#.*//;
-    /^(.+)\s*>\s+([\-\w]*)[,]?(\w*)\s*/;   ##Takeoff data
+    /^(.+)\s*>\s+([\-\w]*)[,]?([\-\w]*)\s*/;   ##Takeoff data
     #print STDERR "$pri : $seg : $2 : $3 : $1\n";
     push @qui, $1;
     $tmp2=$2;
@@ -46,10 +47,11 @@ while(<>){
     }
     $ter[-1]=~s/\-//;       ##Cleaning
     $qui[-1]=~s/\s//g;
+    $qua[-1]=~s/\-/0/;
 }
 
 #Copypaste from upper lines
-printf ("\n%s %s Y %d\n",$pri,$seg,$#ter+1);
-for ($i=0;$i<@ter;$i++){
-    print "$pri $seg   ".lc($ter[$i])."\t\t".lc($qua[$i])."\t\t".lc($qui[$i])."\n";
-}
+printf ("\n%s %s %s %d\n",$pri,$seg,$flg=~/[*+]/ ? 'Y' : 'N',$#ter+1); ##number of items
+	    for ($i=0;$i<@qui;$i++){
+		print "$pri $seg   ".lc($ter[$i])." " x (15-length($ter[$i])).lc($qua[$i])." " x (15-length($qua[$i])).lc($qui[$i])."\n";
+	    }
