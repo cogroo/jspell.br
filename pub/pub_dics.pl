5fbf466e461bd52fe027a2c8b7126e9a9628f9b4
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2005-06-17 13:49:47 +0000

    En
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@2023 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- pub/pub_dics.pl
+++ pub/pub_dics.pl
@@ -34,3 +34,23 @@ $template->param(
 open(DIC_HTML, ">", "$dir/dics.html") or die "$!";
 print {DIC_HTML} $template->output;
 close(DIC_HTML);
+
+### English
+
+my $template = HTML::Template->new(filename => 'lgdics_en.template');
+
+$template->param(
+      MYSPELL => 'myspell-pt.'.$data.'.tar.gz',
+      ISPELL => 'ispell-pt.'.$data.'.tar.gz',
+      ASPELL => 'aspell-pt.'.$data.'.tar.gz',
+      JSPELL => 'jspell-pt.'.$data.'.tar.gz',
+      SMYSPELL => &estat ('myspell-pt.'.$data.'.tar.gz'),
+      SISPELL => &estat ('ispell-pt.'.$data.'.tar.gz'),
+      SASPELL => &estat ('aspell-pt.'.$data.'.tar.gz'),
+      SJSPELL => &estat ('jspell-pt.'.$data.'.tar.gz'),
+
+  );
+
+open(DIC_HTML, ">", "$dir/dics_en.html") or die "$!";
+print {DIC_HTML} $template->output;
+close(DIC_HTML);
