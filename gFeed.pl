9c08f2f2d221eadddc57ed96424f2ed0118e5219
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2006-08-25 17:52:41 +0000

    Experiencia com Atom
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@2230 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- gFeed.pl
+++ gFeed.pl
@@ -1,54 +1,83 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 #Rui Vilela, Linguateca 2006
-#Gera um feed RSS 1.0 para vigiar o lançamento de novas versões do dicionário
+#Gera um feed para o lançamento de novas versões do dicionário
 
-#Falta acumulação de Itens devido a bug, (caracteres acentuadas?)
-
-use XML::RSS;
 use strict;
-use Date::Format; 
+use warnings;
+use XML::Atom::Feed;
+use XML::Atom::Entry;
 use Data::Dumper;
-
-my $date_syn_stamp = time2str("%Y-%m-%dT00:00+00:00", time, 'GMT');
-my $pubdate_stamp = time2str("%C", time, 'GMT');
+use Date::Calc qw(Delta_Days);
 
 my $DIC='/home/natura/download/sources/Dictionaries';
 my $url='http://natura.di.uminho.pt/download/sources/Dictionaries/';
+my @cvs=qw!~/dicionarios/jspell.pt/DIC ~/natura/dicionarios/jspell.pt/DIC!;
+my $cvs;
 
-my $feed='feed.rdf';
+for (@cvs){
+    $cvs=$_ && last if (-d $_);
+}
 
-my $rss = new XML::RSS (version => '1.0',encoding=>'UTF-8');
+my $feedfile='Atom.xml';
 
-#$rss->parsefile("$DIC/$feed") if (-e "$DIC/$feed");
-$rss->channel(
-	      title        => "Natura - Dicionários",
-	      link         => "http://natura.di.uminho.pt",
-	      description  => "Projecto Natura - Actualização dos dicionários portugueses (Jspell/Myspell/Aspell/Ispell)",
-	      pubDate      => $pubdate_stamp,
-	      language   => 'pt-pt',
-	      dc => {
-		  creator    => 'ruivilela@di.uminho.pt',
-		  publisher  => 'ruivilela@di.uminho.pt',
-		  rights     => 'Copyright 2006, Natura / Linguateca',
-	      },
-	      syn => { updatePeriod => 'hourly',
-		       updateBase   => $date_syn_stamp,
-		   },
-	      taxo => [
-		       "http://linguateca.di.uminho.pt",
-		       "http://www.linguateca.pt"
-		       ]
-	      );
+$XML::Atom::DefaultVersion = "1.0";
 
-#pop(@{$rss->{'items'}}) if (@{$rss->{'items'}} == 10); #Número de itens a ficarem
+my $feed = XML::Atom::Feed->new;
 
-#print Dumper $rss->{'items'};
+$feed->version; # 1.0
+$feed->title("Dicionários Opensource para o português");
 
-$rss->add_item(about => time2str('%s',time),
-	       title => 'Nova versão do dicionário português: '. `ls -rt -c1 $DIC/jspell |grep -v 'latest' |sed -e 's/\.tar\.gz//' |tail -n 1`,
-	       link  => $url,
-	       description => 'Nova actualização do dicionário português para Jspell/Myspell/Aspell/Ispell disponível. Ver CHANGELOG para mais informações',
-                );
+my $mlink = XML::Atom::Link->new;
+$mlink->type('text/html');
+$mlink->rel('alternate');
+$mlink->href('http://natura.di.uminho.pt/natura/natura?&topic=Dicion%E1rios');
+$feed->add_link($mlink);
 
-$rss->save("$DIC/$feed") 
+my $author = XML::Atom::Person->new;
+$author->name('Rui Vilela');
+$author->email('ruivilela@di.uminho.pt');
+$feed->author($author);
+
+#######################################################
+my $entry = XML::Atom::Entry->new;
+$entry->title('Nova versão do dicionário português: '. `ls -rt -c1 $DIC/jspell |grep -v 'latest' |sed -e 's/\.tar\.gz//' |tail -n 1`);
+
+my $f=`ls -1t *.gz |grep -v latest|head -n 2`;
+
+$f=s/\n//;
+
+my $D;
+if (/(\d{4})(\d{2})(\d{2})\D+(d{4})(\d{2})(\d{2})/){
+    $D=@-;
+}
+
+my $days = Delta_Days ($D);
+
+my $rcvs;
+foreach (`ls -1 $cvs`){
+    $rcvs=`cd $cvs; cvs diff -D $days`;
+}
+
+print Dumper $rcvs;
+
+$entry->content("Nova actualização do dicionário português para Jspell/Myspell/Aspell/Ispell disponível.\n\n Ver CHANGELOG para mais informações\n");
+
+my $link = XML::Atom::Link->new;
+$link->type('text/html');
+$link->rel('alternate');
+$link->href('http://www.example.com/2003/12/post.html');
+$entry->add_link($link);
+
+$feed->add_entry($entry);
+
+#######################################################
+
+my $xml = $feed->as_xml;
+
+print Dumper $xml;
+
+#$rss->add_item(about => time2str('%s',time),
+#	       description => 'Nova actualização do dicionário português para Jspell/Myspell/Aspell/Ispell disponível. Ver CHANGELOG para mais informações',
+#	       );
+#
