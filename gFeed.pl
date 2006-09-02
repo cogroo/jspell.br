0481fefb599144e31afb5e8062c25bc094de7ae4
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2006-09-02 13:40:22 +0000

    --
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@2240 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- gFeed.pl
+++ gFeed.pl
@@ -13,7 +13,8 @@ use Date::Format qw(time2str);
 use Encode;
 
 
-my $DIC='/home/natura/download/sources/Dictionaries';
+my $DIC='/home/natura/download/sources/Dictionaries/jspell';
+#my $DIC='/home/ruivilela/dd';
 my $url='http://natura.di.uminho.pt/download/sources/Dictionaries/';
 my @cvs=("$ENV{HOME}/dicionarios/jspell.pt/DIC",
 	 "$ENV{HOME}/natura/dicionarios/jspell.pt/DIC");
@@ -26,13 +27,13 @@ for (@cvs){
     }
 }
 
-my $feedfile='Atom.xml';
+my $feedfile='atom.xml';
 
 $XML::Atom::DefaultVersion = "1.0";
 
 my $feed = XML::Atom::Feed->new(Version => 1.0);
 
-$feed->version; # 1.0
+#$feed->version; # 1.0
 $feed->title("Dicionários Opensource para o português");
 
 my $mlink = XML::Atom::Link->new;
@@ -48,9 +49,10 @@ $feed->author($author);
 
 #######################################################
 my $entry = XML::Atom::Entry->new(Version => 1.0);;
-$entry->title(Encode::decode_utf8('Nova versão do dicionário: '. `ls -rt -c1 $DIC/jspell |grep -v 'latest' |sed -e 's/\.tar\.gz//' |tail -n 1`));
 
-my $f=`ls -1t $DIC/jspell/*.gz |grep -v latest|head -n 2`;
+$entry->title(Encode::decode_utf8('Nova versão do dicionário: '. `ls -rt -c1 $DIC |grep -v 'latest' |sed -e 's/\.tar\.gz//' |tail -n 1`));
+
+my $f=`ls -1t $DIC/*.gz |grep -v latest|head -n 2`;
 
 $f=~s/\n//;
 
@@ -64,11 +66,12 @@ if ($f=~/(\d{4})(\d{2})(\d{2})\D+(\d{4})(\d{2})(\d{2})/s){
 
 my $rcvs=Encode::decode_utf8("<p>Alterações efectuadas desde a última actualização (Há $days dia".($days>1 ? "s" : '')."):</p>");
 $rcvs.='='x67;
-$rcvs.="\n\n";
+$rcvs.="\n";
 
 foreach (`ls -1 $cvs/*.dic`){
     $rcvs.= Encode::decode('iso-8859-1',`cd $cvs; cvs diff -D "$days days ago" $_`);
 }
+
 $rcvs=~s/Index:.+\//<b>Ficheiro<\/b>: /g;
 $rcvs=~s/RCS file.+\n//g;
 $rcvs=~s/retrieving revision.+\n//g;
@@ -76,9 +79,9 @@ $rcvs=~s/-r[\d\.]+//g;
 $rcvs=~s/\n/<\/br>\n/g;
 
 #GET LAST ENTRYS
-print Dumper \$rcvs;
+#print Dumper \$rcvs;
 
-$entry->content("$rcvs</br>\n<pre>".Encode::decode_utf8("Ver CHANGELOG para mais informações</pre>\n"));
+$entry->content("$rcvs<pre>".Encode::decode_utf8("Ver CHANGELOG para mais informações")."</pre>\n");
 
 $feed->add_entry($entry);
 
@@ -87,9 +90,41 @@ $feed->add_entry($entry);
 my $xml = $feed->as_xml;
 
 $xml=~s/utf-8/iso-8859-1/i; #Resolver bug do módulo
-my $data=time2str("%Y-%m-%dT%XZ",time,'GMT');
-$xml=~s/(<entry.*)/$1<updated>$data<\/updated>/;
+#print Dumper \$xml;
 
+my $data=time2str("%Y-%m-%dT%XZ",time);
+$xml=~s/<feed.*/$&<updated>$data<\/updated>/;
+$xml=~s/<entry.*/$&<updated>$data<\/updated><published>$data<\/published>/;
+
+my $new = $& if ($xml=~/(<entry.*?<\/entry>)/s);
+#print Dumper \$new;
+
+#Barb
+my $G;
+open $G,"<feedHistory" || goto label1;
+
+$/='</entry>';
+my $c=10;
+my $d=substr($data,0,10);
+my $oldEntry='';
+while (<$G>){
+    next if (/$d/); #Evitar mais do que uma entry por dia
+    next if /^\s$/s;
+    $xml=~s/<\/entry>/$&$_/;
+    last if (--$c==0);
+    $oldEntry.=$_;
+}
+
+$/='\n';
+close $G;
+
+#print Dumper \$xml;
+#print Dumper \$oldEntry;
+label1:
+
+open $G, ">feedHistory" || warn "Not keeping feed history!";
+print $G $new."\n".$oldEntry;
+close $G;
 
 #print Dumper \$xml;
 
@@ -97,4 +132,5 @@ open my $F, ">$DIC/$feedfile" or warn "Não foi possível criar o ficheiro $feed
 print $F $xml;
 close $F;
 
+#xmllint --format ?
 print "$feedfile criado com sucesso!\n" if (-e "$DIC/$feedfile");
