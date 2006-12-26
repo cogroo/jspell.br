d4362ba4239edfeea0fc314c387ddbbd0b9ddc73
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2006-12-26 19:28:24 +0000

    Kanguru irritante
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@4009 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- WORDLIST/verbdiff.pl
+++ WORDLIST/verbdiff.pl
@@ -8,7 +8,7 @@ while(<>){
 
 print "Diferenças de palavras desde a última versão (FALTA ID DA VERSÃO ANTERIOR)"; 
 print "\n\n**Removidas**"; 
-map {s/[<>] //; print} @o; 
+map {print} @o; 
 print "\n\n**Adicionadas**"; 
 map {print} @i;
 
