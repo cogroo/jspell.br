c983ae9c7c4d9bddba9300937996020efa0ecf84
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2007-06-28 08:44:34 +0000

    --
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@4581 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- TESTS/testarPalavrasErradasReEmergentes.pl
+++ TESTS/testarPalavrasErradasReEmergentes.pl
@@ -6,6 +6,7 @@
 use strict;
 use locale;
 use warnings;
+use Text::Diff;
 use Data::Dumper;
 
 my $w=shift;
@@ -15,8 +16,16 @@ die $! unless -d $w;
 die $! unless -e $v;
 
 opendir(my $D, "$w") || die $!;
-foreach my $name (sort readdir($D)) {
-    print "found file: $name\n";
-}
+my @wlist = grep /\d{4}/, sort readdir($D);
+#print Dumper \@wlist;
 closedir($D);
 
+#Calcular listas de palavras removidas e adicionadas
+
+my $f1=shift @wlist;
+for my $f2 (@wlist){
+    my $diff = diff "$w/$f1", "$w/$f2", { STYLE => "Context" };
+    print $diff;
+    die;
+    $f1=$f2;
+}
