46b47ee370affd734ec7d38ce3f6cb8216d44def
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2007-07-09 11:48:54 +0000

    --
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@4670 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- PUBLICAR/estrangeirismos/Estrangeirismos.pl
+++ PUBLICAR/estrangeirismos/Estrangeirismos.pl
@@ -28,15 +28,16 @@ open my $G, "<$lista" || die $!;
 
 my $autor="Proj. Natura (DI-UM), http://natura.di.uminho.pt"; 
 
-my $tit="Lista de estrangeirismos identificados no dicionário de
+my $tit="Lista de estrangeirismos existentes no dicionário de
 português\\footnote{Documento gerado automaticamente a partir do
-dicionário para o Jspell.}\\\\";
+dicionário português para o Jspell.}\\\\";
 
-my $out='\documentclass[twocolumn,10pt]{article}'."\n";
+my $out='\documentclass[10pt]{article}'."\n";
 $out.='\usepackage[latin1]{inputenc}'."\n";
 $out.='\usepackage[portuges]{babel}'."\n";
 $out.='\usepackage[left=1cm,top=2cm,bottom=2cm,right=1cm]{geometry}'."\n";
 $out.='\usepackage{fancyvrb}'."\n";
+$out.='\usepackage{multicol}'."\n";
 $out.='\begin{document}'."\n";
 
 $out.='\title{'.$tit.'}'."\n";
@@ -45,12 +46,15 @@ $out.='\date{\today}'."\n";
 $out.='\maketitle'."\n";
 $out.='\newpage'."\n";
 
+$out.='\begin{multicols}{3}{'."\n";
+
 while (<$G>){
     chomp;
-    $out.= "\n\n\\textbf{$_}\n";
-    &informacaoMorfologica($_) for ($dic->fea($_));
+    my $w=$_;
+    &informacaoMorfologica($_,$w) for ($dic->fea($_));
 }
 close $G;
+$out.="}\n".'\end{multicols}'."\n";
 
 $out.='\end{document}'."\n";
 
@@ -61,12 +65,14 @@ close $F;
 
 
 sub informacaoMorfologica{
-    my ($fea)=@_;
-    $out.="\n".'\begin{Verbatim}'."\n";
+    my ($fea,$w)=@_;
+    $out.="\n\n".'\begin{Verbatim}[commandchars=\\\\\{\}]'."\n";
+    $out.="\\emph{\\textbf{$w}}\n";
     for (keys %$fea) {
        my $data = (exists($data->{$_})&&exists($data->{$_}{$fea->{$_}})?$data->{$_}{$fea->{$_}}:$fea->{$_});
-       my $key = (exists($cats->{$_}))?$cats->{$_}:$_;   
-       $out.="$key: $data"."\n";
+       my $key = (exists($cats->{$_}))?$cats->{$_}:$_;
+       next if (/^rad$/); #ignorar lema
+       $out.=" $key: $data"."\n";
     }
     $out.="\n".'\end{Verbatim}'."\n";
 }
