0e29ca2930a1cbd502f190d034b8ac8f92bce5a4
ruivilela <ruivilela@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2007-07-14 15:56:02 +0000

    --
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@4708 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

---
+++ DOC/processarMeta.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+#Rui Vilela, 2007
+
+#Processa as variáveis contidas em $, usar com a makefile
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $t=shift;
+my $f=shift;
+my $g=shift;
+
+#ler metadados
+
+open my $T, "<metadados.$t" || die $!;
+my %mt;
+
+while(<$T>){
+    chomp;
+    if (/^(\S+) (.+)$/){
+	$mt{$1}=$2;
+    }
+}
+
+close $T;
+
+open my $F, "<$f" || die $!;
+my $o;
+
+while(<$F>){
+    $o.=$_;
+}
+
+close $F;
+
+$o=~s/--/\t\t\t\t----------/g;
+
+for (keys %mt){
+    my $u=$mt{$_};
+    if ($u=~s/\`//g){
+	$u=`$u`;
+    }
+    $o=~s/\$$_\$/$u/gs;
+}
+
+$o=~s/\$\$/ /g;
+$o=~s/\|/\n/g;
+
+print "$o\n";
+
+#open my $G, ">$g" || die $!;
+
+#while(<$G>){
+#
+#}
+
+#close $G;
+
+#unlink $f;
