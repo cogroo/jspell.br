8b1067338ff5a288bcaa918982a24bb39b626aaa
ambs <ambs@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2010-10-28 17:16:35 +0000

    Foo
    git-svn-id: https://natura.di.uminho.pt/svn/main/dicionarios/jspell.pt@9126 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- HUNSPELL/genDicHunspell.pl
+++ HUNSPELL/genDicHunspell.pl
@@ -12,7 +12,9 @@ use locale;
 
 while(<>){
     chomp;
+	next if m!^\s*$!;
     my ($word, $morf, $flags) = split m!/!;
+    die $_ unless $word && $morf && defined($flags);
     print "$word".($flags=~/^\s*$/ ? '':'/')."$flags\t[$morf]\n";
 #    print "$word/$flags\t[$morf]\n";
 }
